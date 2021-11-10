553e0cc6cceed8ba5a9332491593cd879d988889
imaniclarke4 <imaniclarke97@gmail.com>
2021-11-09 23:37:36 -0500

    Added initial starter files

---
+++ world.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", function () {
+    const lookup = document.querySelector("#lookup");
+    const city_button = document.querySelector("#city_lookup");
+  
+    city_button.addEventListener("click", () => {
+      performLookup("city");
+    });
+  
+    lookup.addEventListener("click", () => {
+      performLookup("country");
+    });
+  });
+  
+  async function performLookup(type) {
+    const fieldinput = document.querySelector("#country").value;
+    const fieldresult = document.querySelector("#result");
+  
+    let url = "";
+    if (type === "country") {
+      url = `world.php?country=${fieldinput}&context=''`;
+    } else {
+      url = `world.php?country=${fieldinput}&context=cities`;
+    }
+  
+    const response = fetchData(url);
+    fieldresult.innerHTML = response;
+  }
+  
+  async function fetchData(url) {
+    return await fetch(url).then((response) => {
+      let data = response.text();
+      return data;
+    });
+  }
--- world.php
+++ world.php
@@ -1,17 +1,69 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
+// Retrieve query strings
+$country = $_GET['country'];
+$cityParam = $_GET['context'];
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
 
+// Make SQL query based on query string
+$stmt = getQuery($conn, $country, $cityParam);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+/**
+ * Makes queries to SQL based on the query string
+ */
+function getQuery($conn, $country, $cityParam){
+  if($cityParam != "cities"){
+    return $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  }else{
+    return $conn->query("SELECT cities.name, cities.district,cities.population 
+                         FROM countries LEFT JOIN cities ON countries.code = cities.country_code 
+                         WHERE countries.name LIKE '%$country%'");
+  }
+}
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+
+<table>
+<?php
+$cityStr = ''; 
+$headerStr = '';
+$headerStr .= '<tr>';
+$headerStr .= '<th>Name</th>';
+  if($cityParam != "cities"){
+    $headerStr .= '<th>Continent</th>';
+    $headerStr .= '<th>Independence</th>';
+    $headerStr .= '<th>Head of State</th>';
+  }else{
+    $headerStr .= '';
+    $headerStr .= '<th>District</th>';
+    $headerStr .= '<th>Population</th>';
+  }
+  $headerStr .= '</tr>';
+
+  echo $headerStr;
+  foreach ($results as $row):
+    if($cityParam != "cities"){
+      $cityStr .= "<tr>";
+      $cityStr .= "<td>{$row['name']}</td>";
+      $cityStr .= "<td>{$row['continent']}</td>";
+      $cityStr .= "<td>{$row['independence_year']}</td>";
+      $cityStr .= "<td>{$row['head_of_state']}</td>";
+      $cityStr .= "</tr>";
+    }else{
+      $cityStr .= "<tr>";
+      $cityStr .= "<td>{$row['name']}</td>";
+      $cityStr .= "<td>{$row['district']}</td>";
+      $cityStr .= "<td>{$row['population']}</td>";
+      $cityStr .= "</tr>";
+    }
+    echo $cityStr;
+?>
 <?php endforeach; ?>
-</ul>
+</table>
+
+
